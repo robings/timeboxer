807c2e886f75e08dda7493e68d945ed4d4074c6b
robings <r_gordon_stables@hotmail.com>
2020-01-17 22:46:19 +0000

    styled times up message

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function timebox(minutesToTime) {
 	//alert ('You picked'+minutesToTime);
-		timeoutVar=setTimeout(timesUp, (minutesToTime*60000));
+		timeoutVar=setTimeout(timesUp, (minutesToTime*600));
 	document.getElementById('sectionBox').innerHTML="<div class=\"timerDiv\">Don't close this browser window I'm timing for "+minutesToTime+" minutes</div><button onclick=\"cancelIt(timeoutVar);\">cancel</button>"
 		
 }
 
 function timesUp() {
-	document.getElementById('sectionBox').innerHTML="<div class=\"endTimeMessage\"><img src=\"images/pygmy-hippos.jpg\" alt=\"two pigmy hippos\" />Time to consult a fellow hippo</div>";
+	document.getElementById('sectionBox').innerHTML="<div class=\"endTimeMessage\">Time to consult a fellow hippo<button onclick=\"javascript:document.location.reload(false)\">reset</button></div>";
 }
 
 function cancelIt(cancelTime) {
 	clearTimeout(cancelTime);
-	document.location.reload(true);
+	document.location.reload(false);
 }
