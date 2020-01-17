d4276fdac0db4b45f64e6a9fb9dd1e1467f80a12
robings <r_gordon_stables@hotmail.com>
2020-01-17 21:54:03 +0000

    added change to display and cancel button

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 function timebox(minutesToTime) {
-	alert ('You picked'+minutesToTime);
-	
-	setTimeout(timesUp, (1000));
+	//alert ('You picked'+minutesToTime);
+		timeoutVar=setTimeout(timesUp, (minutesToTime*60000));
+	document.getElementById('sectionBox').innerHTML="<div class=\"timerDiv\">Don't close this browser window I'm timing for "+minutesToTime+" minutes</div><button onclick=\"cancelIt(timeoutVar);\">cancel</button>"
+		
 }
 
 function timesUp() {
-	alert ('Time to consult a fellow hippo')
+	alert ('Time to consult a fellow hippo');
+}
+
+function cancelIt(cancelTime) {
+	clearTimeout(cancelTime);
 }
