6a6b0664a9bece3a419f8fbbf490c650636f11ab
robings <r_gordon_stables@hotmail.com>
2020-01-18 22:32:39 +0000

    restyling of buttons, moved sound code before display code

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function timebox(minutesToTime) {
 	//alert ('You picked'+minutesToTime);
 		timeoutVar=setTimeout(timesUp, (minutesToTime*60000));
-	document.getElementById('sectionBox').innerHTML="<div class=\"timerDiv\">Don't close this browser window I'm timing for "+minutesToTime+" minutes</div><button onclick=\"cancelIt(timeoutVar);\">cancel</button>"
+	document.getElementById('sectionBox').innerHTML="<div class=\"timerDiv\">Don't close this browser window I'm timeboxing for "+minutesToTime+" minutes<br><button onclick=\"cancelIt(timeoutVar);\">cancel</button></div>"
 		
 }
 
 function timesUp() {
 	window.focus();
-	document.getElementById('sectionBox').innerHTML="<div class=\"endTimeMessage\">Time to consult a fellow hippo<br><button onclick=\"javascript:document.location.reload(false)\">reset</button></div>";
 	var alarmChoice=document.getElementById("alarms").value;
 	var alarmSound=0;
 	if (alarmChoice!="noSound") {
@@ -27,9 +26,16 @@ function timesUp() {
 		}
 		alarmSound.play();
 	}
+	
+	document.getElementById('sectionBox').innerHTML="<div class=\"endTimeMessage\">Time to consult a fellow hippo<br><button onclick=\"resetIt()\">reset</button></div>";
 }
 
 function cancelIt(cancelTime) {
 	clearTimeout(cancelTime);
+	resetIt();
+}
+
+function resetIt() {
 	document.getElementById('sectionBox').innerHTML="<div id=\"tenMinutes\" class=\"tenMins\" onclick=\"timebox(10);\">give me 10 minutes</div><div id=\"fifteenMinutes\" class=\"fifteenMins\" onclick=\"timebox(15);\">give me 15 minutes</div>"
+	
 }
