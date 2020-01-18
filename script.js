8f5be42df07d72b8504a9aae6fc1d81e570cddbe
robings <r_gordon_stables@hotmail.com>
2020-01-18 21:05:48 +0000

    added playing an alarm sound

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function timebox(minutesToTime) {
 function timesUp() {
 	window.focus();
 	document.getElementById('sectionBox').innerHTML="<div class=\"endTimeMessage\">Time to consult a fellow hippo<br><button onclick=\"javascript:document.location.reload(false)\">reset</button></div>";
+	var alarmSound=new Audio('sounds/hippo.mp3');
+	alarmSound.play();
 }
 
 function cancelIt(cancelTime) {
