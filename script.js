1bebcb1f8d5506362c12033083297febf1bfe688
robings <r_gordon_stables@hotmail.com>
2020-01-18 22:14:20 +0000

    alarm sound selector implemented

--- script.js
+++ script.js
@@ -8,11 +8,28 @@ function timebox(minutesToTime) {
 function timesUp() {
 	window.focus();
 	document.getElementById('sectionBox').innerHTML="<div class=\"endTimeMessage\">Time to consult a fellow hippo<br><button onclick=\"javascript:document.location.reload(false)\">reset</button></div>";
-	var alarmSound=new Audio('sounds/hippo.mp3');
-	alarmSound.play();
+	var alarmChoice=document.getElementById("alarms").value;
+	var alarmSound=0;
+	if (alarmChoice!="noSound") {
+		switch(alarmChoice) {
+			case "hippo":
+					alarmSound=new Audio('sounds/hippo.mp3');
+					break;
+			case "hippoChord":
+					alarmSound=new Audio('sounds/hippoChordReverb.mp3');
+					break;
+			case "guitarChord":
+					alarmSound=new Audio('sounds/guitarChord.mp3');
+					break;
+			case "rockChord":
+					alarmSound=new Audio('sounds/rockChord.mp3');
+					break;		
+		}
+		alarmSound.play();
+	}
 }
 
 function cancelIt(cancelTime) {
 	clearTimeout(cancelTime);
-	document.location.reload(false);
+	document.getElementById('sectionBox').innerHTML="<div id=\"tenMinutes\" class=\"tenMins\" onclick=\"timebox(10);\">give me 10 minutes</div><div id=\"fifteenMinutes\" class=\"fifteenMins\" onclick=\"timebox(15);\">give me 15 minutes</div>"
 }
