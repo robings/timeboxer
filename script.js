e06bbdc70b0bcbc78fbf448f2d4a5d98736e2226
robings <r_gordon_stables@hotmail.com>
2020-03-14 21:15:26 +0000

    added slider for custom time, and div to display current value of slider

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ document.getElementById('tenMinutes').addEventListener('click', (e)=>{
 document.getElementById('fifteenMinutes').addEventListener('click', (e)=>{
 	timebox(15)
 })
+
+document.getElementById('customTimeDisplay').textContent = document.querySelector('input[type="range"]').value
+document.querySelector('input[type="range"]').addEventListener('input', ()=>{
+	document.getElementById('customTimeDisplay').textContent = document.querySelector('input[type="range"]').value
+})
